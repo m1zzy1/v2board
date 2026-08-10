e0509d7afe431474c442efe1fc1f116d6f73c130
Sororain <m15055510xx@gmail.com>
2026-08-10 21:13:33 +0800

    feat: 增加开关

--- app/Http/Controllers/V1/Admin/ConfigController.php
+++ app/Http/Controllers/V1/Admin/ConfigController.php
@@ -110,6 +110,7 @@ class ConfigController extends Controller
                 'reset_traffic_method' => (int)config('v2board.reset_traffic_method', 0),
                 'surplus_enable' => (int)config('v2board.surplus_enable', 1),
                 'allow_new_period' => (int)config('v2board.allow_new_period', 0),
+                'new_period_min_usage_enable' => (int)config('v2board.new_period_min_usage_enable', 0),
                 'new_period_min_usage' => (int)config('v2board.new_period_min_usage', 100),
                 'new_order_event_id' => (int)config('v2board.new_order_event_id', 0),
                 'renew_order_event_id' => (int)config('v2board.renew_order_event_id', 0),
