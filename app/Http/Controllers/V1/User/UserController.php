3435d6f531cbb8c79f9d00671cf99be5ed1e5df6
Sororain <m15055510xx@gmail.com>
2026-08-10 21:17:29 +0800

    fix: 回退开关功能

--- app/Http/Controllers/V1/User/UserController.php
+++ app/Http/Controllers/V1/User/UserController.php
@@ -98,9 +98,7 @@ class UserController extends Controller
             if (!$user) {
                 abort(500, __('The user does not exist'));
             }
-            $minUsage = (int)config('v2board.new_period_min_usage_enable', 0)
-                ? (int)config('v2board.new_period_min_usage', 100)
-                : 100;
+            $minUsage = (int)config('v2board.new_period_min_usage', 100);
             $usedUsage = $user->u + $user->d;
             $usedPercent = $user->transfer_enable > 0 ? $usedUsage * 100 / $user->transfer_enable : 0;
             if ($usedPercent < $minUsage) {
@@ -354,7 +352,6 @@ class UserController extends Controller
         $userService = new UserService();
         $user['reset_day'] = $userService->getResetDay($user);
         $user['allow_new_period'] = config('v2board.allow_new_period', 0);
-        $user['new_period_min_usage_enable'] = config('v2board.new_period_min_usage_enable', 0);
         $user['new_period_min_usage'] = config('v2board.new_period_min_usage', 100);
         return response([
             'data' => $user
